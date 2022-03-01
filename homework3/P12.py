834609034a8009d0b37327ffa3bc11bdec6c419a
Mr.Chen <92870871+Mr-Chen-zihao@users.noreply.github.com>
2022-03-01 23:44:29 +0800

    Create P12.py

---
+++ homework3/P12.py
@@ -0,0 +1,13 @@
+#  homework3--P12
+
+from socket import *
+serverName = 'P12servername'
+serverPort = 12000
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind(('',serverPort))
+serverSocket.listen(1)
+connectionSocket, addr = serverSocket.accept()
+while 1:
+  sentence = connectionSocket.recv(1024)
+  print 'From Server:', sentence, '/n'
+serverSocket.close() 
